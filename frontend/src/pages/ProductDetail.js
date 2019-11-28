9a930ee3a651880ec56eabff13991f34e7a69f75
passwd10 <parkinseo94@naver.com>
2019-11-28 19:34:50 +0900

    Add purchase and carlist button

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,33 +1,37 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 
-import productStore from "../stores/productStore";
+import productStore, { cartList } from "../stores/productStore";
 
 function ProductDetail() {
     const { productId } = useParams();
-    const product = productStore.getProduct(productId);
+    const { title, price, categoryId, categoryName, img, detail } = productStore.getProduct(productId);
+
+    const addCartList = () => {
+        cartList.createCartList(title, categoryId, categoryName);
+    }
 
     return (
         <>
-            <h1>{product.title}</h1>
-            <h3>가격 : {product.price} 원</h3>
+            <h1>{title}</h1>
+            <h3>가격 : {price} 원</h3>
             <img
-                src={product.img}
+                src={img}
                 alt=""
                 width="50%"
             />
-            <h2>카테고리 : {product.category} </h2>
-            <p>상품설명 : {product.detail}</p>
-            
+            <h2>카테고리 : {categoryName} </h2>
+            <p>상품설명 : {detail}</p>
+
             <select width='300px' id='selectPurchaseOpt'>
                 <option value={undefined} key={0}>옵션을 선택하세요</option>
-                <option value={undefined} key={1}>기본 / 가격 : {product.price}원</option>
+                <option value={undefined} key={1}>기본 / 가격 : {price}원</option>
             </select>
 
-            {/* <Link to={`/order/purchaseRequest`}> */}
+            <Link to={`/order/purchaseRequest/${productId}`}>
                 <button>구매하기</button>
-            {/* </Link> */}
-            <button>장바구니</button>
+            </Link>
+            <button onClick={addCartList}>장바구니</button>
         </>
     );
 }
