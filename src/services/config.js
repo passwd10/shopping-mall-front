bfeceeb122905687ee3b1833787134e2c9f1be91
passwd10 <parkinseo94@naver.com>
2020-02-05 18:24:12 +0900

    Add build script

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'answer-shopping-mall.netlify.com';
-export const API_SERVER_URL = 'https://shopping-mall-api.herokuapp.com/';
+const SERVER_URL = 'https://answer-shopping-mall.netlify.com';
+export const API_SERVER_URL = 'https://shopping-mall-api.herokuapp.com';
 
 export default SERVER_URL;
--- src/services/productsCategoryService.js
+++ src/services/productsCategoryService.js
@@ -6,6 +6,7 @@ const URL = API_SERVER_URL + '/productsCategory';
 
 const getCategories = async () => {
   const { data } = await axios.get(URL);
+  console.log('data', data);
   return data;
 };
 
